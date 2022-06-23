d43a38d8cfd388ed75d32644c5cf515b82187f8c
Xdit_¡X <107161761+XCotnana@users.noreply.github.com>
2022-06-23 18:57:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,21 +27,21 @@ global.premium = ['6285641476033'] //ubah jadi nomor mu, note tanda ' gausah di 
 global.ownernomer = '6285641476033' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'IG @hai_om.qw' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Xdit-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Xdit-MD' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'X-ZC' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'X-ZC' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'X-ZC' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://instagram.com/hai_om.qw'
 global.ig = 'https://instagram.com/hai_om.qw'
-global.mygc = 'https://syo.vercel.app/x¡/whatsapp'
+global.mygc = 'http://tiny.cc/X-ZC'
 global.myweb = 'https://instagram.com/hai_om.qw'
 global.email = 'xcomunityx77@gmail.com'
 global.region = 'Mars'
 
 //—————「 Set Wm 」—————//
-global.packname = 'Xdit-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = 'X-ZC' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.author = 'IG @hai_om.qw' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
